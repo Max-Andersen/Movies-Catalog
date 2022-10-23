cc00464894e6760a4fefe97f41f8d4ec1dcffa74
Toloknov Maxim <maxim25072003@gmail.com>
2022-10-23 23:18:39 +0700

    Sign-Up Screen appearance finished

--- app/src/main/java/com/example/moviecatalog/signUp/SignUpScreen.kt
+++ app/src/main/java/com/example/moviecatalog/signUp/SignUpScreen.kt
@@ -1,14 +1,8 @@
 package com.example.moviecatalog.signUp
 
-import android.app.Activity
 import android.app.DatePickerDialog
-import android.icu.text.SimpleDateFormat
 import android.widget.DatePicker
-import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ButtonDefaults
@@ -21,11 +15,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.intl.Locale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
@@ -35,7 +28,6 @@ import com.example.moviecatalog.ui.theme.MovieCatalogTheme
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.moviecatalog.SetOutlinedTextField
 import com.example.moviecatalog.isAllTextFieldsFull
-import com.google.android.material.datepicker.MaterialDatePicker
 import java.util.*
 
 
@@ -133,6 +125,67 @@ fun DatePickerView( mDate: MutableState<String> ) {
     }
 }
 
+
+@Composable
+fun ChoseGender(model: SignUpViewModel, gender: MutableState<String>){
+    Box(modifier = Modifier
+        .clip(RoundedCornerShape(1.dp))
+        .border(1.dp, MaterialTheme.colorScheme.secondary, RoundedCornerShape(4.dp))
+        .height(47.dp)
+    )
+    {
+        Row(modifier = Modifier.fillMaxSize(),
+            ) {
+
+            OutlinedButton(onClick = { model.ChangeGerder(gender, "0") },
+                colors = ButtonDefaults.outlinedButtonColors(
+                    backgroundColor = if (gender.value == "0") MaterialTheme.colorScheme.primary
+                    else MaterialTheme.colorScheme.background,
+                ),
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxSize(),
+                border = BorderStroke(0.5.dp, MaterialTheme.colorScheme.secondary),
+                shape = RoundedCornerShape(
+                    topStart = 4.dp,
+                    bottomStart = 4.dp,
+                    topEnd = 0.dp,
+                    bottomEnd = 0.dp
+                )
+                )
+            {
+                Text(text = "Мужчина",
+                    color = if (gender.value == "0") MaterialTheme.colorScheme.onPrimary
+                    else MaterialTheme.colorScheme.secondary,
+
+                    )
+            }
+
+            OutlinedButton(onClick = { model.ChangeGerder(gender, "1")  },
+                colors = ButtonDefaults.outlinedButtonColors(
+                    backgroundColor = if (gender.value == "1") MaterialTheme.colorScheme.primary
+                    else MaterialTheme.colorScheme.background,
+                ),
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxSize(),
+                border = BorderStroke(0.5.dp, MaterialTheme.colorScheme.secondary),
+                shape = RoundedCornerShape(
+                    topStart = 0.dp,
+                    bottomStart = 0.dp,
+                    topEnd = 4.dp,
+                    bottomEnd = 4.dp
+                )
+                ) {
+                Text(text = "Женщина",
+                    color = if (gender.value == "1") MaterialTheme.colorScheme.onPrimary
+                    else MaterialTheme.colorScheme.secondary,)
+            }
+        }
+    }
+}
+
+
 @Composable
 fun SignUpScreen(model: SignUpViewModel = viewModel()){ //
     MovieCatalogTheme {
@@ -142,21 +195,24 @@ fun SignUpScreen(model: SignUpViewModel = viewModel()){ //
         val password = remember { mutableStateOf("") }
         val passwordConfirmation = remember { mutableStateOf("") }
         val mDate = remember { mutableStateOf("") }
+        val gender = remember { mutableStateOf("") }
 
         Surface(
             modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background
+            color = MaterialTheme.colorScheme.background,
         ) {
-            Column(modifier = Modifier
-                .fillMaxSize()
-                .padding(
-                    top = 56.dp,
-                    start = 16.dp,
-                    end = 16.dp
-                ),
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(
+                        top = 56.dp,
+                        start = 16.dp,
+                        end = 16.dp
+                    )
+                    .verticalScroll(rememberScrollState()),
                 verticalArrangement = Arrangement.spacedBy(16.dp),
-                //horizontalAlignment = Alignment.CenterHorizontally
-            ) {
+
+                ) {
 
                 Image(
                     painter = painterResource(id = R.drawable.logo_group),
@@ -179,28 +235,30 @@ fun SignUpScreen(model: SignUpViewModel = viewModel()){ //
                 SetOutlinedTextField(password, "Пароль")
                 SetOutlinedTextField(passwordConfirmation, "Подтвердите пароль")
 
-
-
                 DatePickerView(mDate)
+
+                ChoseGender(model = model, gender = gender)
+
+                Spacer(modifier = Modifier.size(16.dp))
 
                 OutlinedButton(
                     onClick = { /*TODO*/ },
-                    enabled = isAllTextFieldsFull(login, email, name, password, passwordConfirmation, mDate),
+                    enabled = isAllTextFieldsFull(login, email, name, password, passwordConfirmation, mDate, gender),
                     modifier = Modifier
                         .fillMaxWidth()
                         .height(53.dp),
                     colors = ButtonDefaults.outlinedButtonColors(
-                        backgroundColor = if (isAllTextFieldsFull(login, email, name, password, passwordConfirmation, mDate)) MaterialTheme.colorScheme.primary
+                        backgroundColor = if (isAllTextFieldsFull(login, email, name, password, passwordConfirmation, mDate, gender)) MaterialTheme.colorScheme.primary
                                             else MaterialTheme.colorScheme.background,
                         contentColor = MaterialTheme.colorScheme.primary,
                         disabledContentColor = MaterialTheme.colorScheme.background
                     ),
-                    border = BorderStroke(1.dp, if (isAllTextFieldsFull(login, email, name, password, passwordConfirmation, mDate)) MaterialTheme.colorScheme.primary
+                    border = BorderStroke(1.dp, if (isAllTextFieldsFull(login, email, name, password, passwordConfirmation, mDate, gender)) MaterialTheme.colorScheme.primary
                                                 else MaterialTheme.colorScheme.onSecondary)
 
                 ) {
                     Text(text = "Зарегистрироваться",
-                        color = if (isAllTextFieldsFull(login, email, name, password, passwordConfirmation, mDate)) MaterialTheme.colorScheme.onPrimary
+                        color = if (isAllTextFieldsFull(login, email, name, password, passwordConfirmation, mDate, gender)) MaterialTheme.colorScheme.onPrimary
                                 else MaterialTheme.colorScheme.primary,
                     )
                 }
@@ -213,7 +271,7 @@ fun SignUpScreen(model: SignUpViewModel = viewModel()){ //
                 ) {
                     Text(text = "У меня уже есть аккаунт", color = MaterialTheme.colorScheme.primary)
                 }
-
+                Spacer(modifier = Modifier.size(32.dp))
             }
         }
     }
--- app/src/main/java/com/example/moviecatalog/signUp/SignUpViewModel.kt
+++ app/src/main/java/com/example/moviecatalog/signUp/SignUpViewModel.kt
@@ -1,7 +1,12 @@
 package com.example.moviecatalog.signUp
 
+import androidx.compose.runtime.MutableState
 import androidx.lifecycle.ViewModel
 
 class SignUpViewModel : ViewModel() {
-
+    fun ChangeGerder(currentGender: MutableState<String>, buttonType: String){ // 0 - male   1 - female
+        if (currentGender.value != buttonType){
+            currentGender.value = buttonType
+        }
+    }
 }
